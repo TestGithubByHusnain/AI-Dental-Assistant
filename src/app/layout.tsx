2341283ce54a52d5f9aaf82bb47a27023137eae0
Husnain Ali <161426089+TestGithubByHusnain@users.noreply.github.com>
2025-10-23 15:03:04 +0500

    Revert "Add Admin-page."

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-// import UserSync from "@/components/UserSync";
-import TanStackProvider from "@/components/providers/TanStackProvider";
-import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,7 +11,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "DentWise - AI Powered Dental Assistant",
   description:
@@ -28,27 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TanStackProvider>
-      <ClerkProvider
-        appearance={{
-          variables: {
-            colorPrimary: "#e78a53",
-            colorBackground: "#f3f4f6",
-            colorText: "#111827",
-            colorTextSecondary: "#6b7280",
-            colorInputBackground: "#f3f4f6",
-          },
-        }}
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <html lang="en">
-          <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>
-            {/* this is done in the home page component */}
-            {/* <UserSync /> */}
-            <Toaster />
-            {children}
-          </body>
-        </html>
-      </ClerkProvider>
-    </TanStackProvider>
+        {children}
+      </body>
+    </html>
   );
 }
